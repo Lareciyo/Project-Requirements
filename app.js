9131ebda23902c3fd3e9b6e6a562546922694d2e
Lareciyo <lmcneal@perseverenow.org>
2025-07-11 16:45:12 -0500

    third comment

--- app.js
+++ app.js
@@ -63,8 +63,8 @@ function setupDragHandlers(){
 //=============================When dragging starts listen
 
 function handleDragStart(e){
-    const toRow = parseInt(e.target.dataset.row);
-    const toCol = parseInt(e.target.dataset.col);
+    const toRow = parseInt(square.dataset.row);
+    const toCol = parseInt(square.dataset.col);
     const piece = boardState[row][col];
     //==========================currentPlayers piece
     //drag only your own piece
@@ -79,7 +79,7 @@ function handleDragStart(e){
 }
 
 //=======================dropped piece
-function handleDroppem(e){
+function handleDrop(event){
     const toRow = parseInt(e.currentTarget.dataset.row);
     const toCol = parseInt(e.currentTarget.dataset.col);
     const [fromRow, fromCol] = dragFrom;
@@ -87,7 +87,41 @@ function handleDroppem(e){
     boardState[fromRow][fromCol] = ''; //clear old spot
     //check moves
     if(isMoveLegal(fromRow, fromCol, toRow, toCol)){
+        boardState[toRow][toCol] = draggedPiece; //move piece
+        boardState[fromRow][fromCol] = ''; //clear old spot
+        //rewarding the pawn for making it across the board upgrade to a queen
+        if(draggedPiece === '♙' && toRow === 0)//if this white pawn reaches the top row it will be replaced with a white queen
+            boardState[toRow][toCol] = '♕';
+        if(draggedPiece === '♟' && toRow === 7)//if this black pawn reaches the bottom row it will be replaced with a black queen
+            boardState[toRow][toCol] = '♛';
 
+        createBoard(); //updates the board with queen info
+
+        //switch to the other player
+        currentPlayer = currentPlayer === "White" ? "Black" : "White";//if currentPlayer is white, it becomes black
+        playerDisplay.innerText = currentPlayer;//displays whose turn it is next
+        infoDisplay.innerText = ''; //clears out old messages
+    }else{
+        infoDisplay.innerText = "Frivolous Move, Are You Nervous!";
     }
+
+    draggedPiece = null; //reset dragged piece and it's previous position
+    dragFrom = null; //resets the drag-and-drop-logic so the board should be ready for the next turn.
 }
+
+//======================RESET BUTTON LOGIC
+
+resetBtn.addEventListener('click', () => {//when the reset button is clicked , it clears the board and starts a new game
+    selectedSquare = null;//clear out the square the player clicked on
+    draggedPiece = null;
+    dragFrom = null;//clear out the square memory
+    currentPlayer = 'White';//the game always starts with white
+    playerDisplay.innerText = currentPlayer;//updates the player display box, keeps the user informed
+    infoDisplay.innerText = '';//clears out any old messages
+
+    initBoardState();//loading a fresh chessboard
+    createBoard();//the function redraws the board on the screen
+
+})
+//===================SPOILS OF WAR
 
